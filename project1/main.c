f0ec65fd17ba66cc6d80faccc1a41b83ae61cc90
acht7111020 <ciindy321123@yahoo.com.tw>
2015-04-02 14:36:38 +0800

    Initial Commit

---
+++ project1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  project1
+//
+//  Created by Mac on 2015/4/2.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
